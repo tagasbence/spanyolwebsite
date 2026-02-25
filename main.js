96eee15f1e3c50883bc8faa549afdf44d7e09288
Boifailure <sadcarot0@gmail.com>
2026-02-25 13:55:05 +0100

    light mode 2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const title = document.getElementById("heroTitle");
 video.addEventListener("timeupdate", () => {
     if (video.currentTime > video.duration * 0.1) {
         title.style.opacity = "1";
-        title.style.transform = translateY(0);
+        //title.style.transform = translateY(0);
     }
 });
 
@@ -17,5 +17,13 @@ const lightModeButton = document.getElementById("lightMode");
 
 function ToggleLightmode() {
     lightMode = !lightMode;
-
+    console.log(`lightmode: ${lightMode}`);
+    if(lightMode) {
+        lightModeButton.classList.add("lightModeState-light");
+        lightModeButton.classList.remove("lightModeState-dark");
+    }
+    else {
+        lightModeButton.classList.remove("lightModeState-light");
+        lightModeButton.classList.add("lightModeState-dark");
+    }
 }
